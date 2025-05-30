def2b90f977e4e0df1e752b3f1ce3c55d82f0dfe
Anton Acc <antonxacc@gmail.com>
2025-05-30 17:48:59 +0300

    Add AllowedEmailDomains constraint

--- src/Service/ForbiddenWordsInCodeService.php
+++ src/Service/ForbiddenWordsInCodeService.php
@@ -1,11 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service;
 
-class ForbiddenWordsInCodeService implements ForbiddenWordsServiceInterface
+readonly class ForbiddenWordsInCodeService implements ForbiddenWordsServiceInterface
 {
+    public function __construct(
+        private array $forbiddenWordList = ['admin', 'root', 'superuser']
+    ) {}
+
     public function check(string $word): bool
     {
+        foreach ($this->forbiddenWordList as $forbiddenWord) {
+            if (mb_strpos(mb_strtolower($word), mb_strtolower($forbiddenWord)) !== false) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
